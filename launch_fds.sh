34ed6b2e28d281ddd4ea898f2e938725cc55e3ba
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2023-02-24 15:38:34 +0100

    Update launch_fds.sh

--- launch_fds.sh
+++ launch_fds.sh
@@ -1,3 +1,4 @@
+cat > run << _EOF
 #! /bin/bash
 # $MPI, $Threads, $FDS_File_path
 # 
@@ -5,3 +6,4 @@ echo $FDS_File_path
 echo $MPI
 echo $Threads
 mpiexec -n 2 fds $FDS_File_path -p $MPI -o $Threads
+_EOF
